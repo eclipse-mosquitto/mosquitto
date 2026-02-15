42d49fc095ae840a0452b76e5e2cf6335a707db1
Roger A. Light <roger@atchoo.org>
2026-02-15 22:05:06 +0000

    Fix outgoing maximum-packet-size limit check.
    Closes #3503. Thanks to Julian Graf.

--- test/broker/12-prop-maximum-packet-size-publish-qos2.py
+++ test/broker/12-prop-maximum-packet-size-publish-qos2.py
@@ -17,13 +17,13 @@ subscribe_packet = mosq_test.gen_subscribe(mid, "test/topic", 2, proto_ver=5)
 suback_packet = mosq_test.gen_suback(mid, 2, proto_ver=5)
 
 mid=1
-publish1_packet = mosq_test.gen_publish(topic="test/topic", mid=mid, qos=2, payload="12345678901234567890", proto_ver=5)
+publish1_packet = mosq_test.gen_publish(topic="test/topic", mid=mid, qos=2, payload="1234", proto_ver=5)
 pubrec1_packet = mosq_test.gen_pubrec(mid, proto_ver=5)
 pubrel1_packet = mosq_test.gen_pubrel(mid, proto_ver=5)
 pubcomp1_packet = mosq_test.gen_pubcomp(mid, proto_ver=5)
 
 mid=2
-publish2_packet = mosq_test.gen_publish(topic="test/topic", mid=mid, qos=2, payload="7890", proto_ver=5)
+publish2_packet = mosq_test.gen_publish(topic="test/topic", mid=mid, qos=2, payload="789", proto_ver=5)
 pubrec2_packet = mosq_test.gen_pubrec(mid, proto_ver=5)
 pubrel2_packet = mosq_test.gen_pubrel(mid, proto_ver=5)
 pubcomp2_packet = mosq_test.gen_pubcomp(mid, proto_ver=5)
--- test/lib/11-prop-oversize-packet.py
+++ test/lib/11-prop-oversize-packet.py
@@ -14,8 +14,8 @@ connect_packet = mosq_test.gen_connect("publish-qos0-test", keepalive=keepalive,
 props = mqtt5_props.gen_uint32_prop(mqtt5_props.PROP_MAXIMUM_PACKET_SIZE, 30)
 connack_packet = mosq_test.gen_connack(rc=0, proto_ver=5, properties=props)
 
-bad_publish_packet = mosq_test.gen_publish("pub/test", qos=0, payload="0123456789012345678", proto_ver=5)
-publish_packet = mosq_test.gen_publish("pub/test", qos=0, payload="012345678901234567", proto_ver=5)
+bad_publish_packet = mosq_test.gen_publish("pub/test", qos=0, payload="123456789012345678", proto_ver=5)
+publish_packet = mosq_test.gen_publish("pub/test", qos=0, payload="12345678901234567", proto_ver=5)
 
 disconnect_packet = mosq_test.gen_disconnect()
 
